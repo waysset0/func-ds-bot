95af047af0bf8d24410903c3ddcbdb19e6e6c8ca
LunarDev <101055131+waysset0@users.noreply.github.com>
2025-01-29 23:06:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 settings = {
-	'TOKEN': 'MTE5MzU4NTc3NjIyNzc5NTA2NQ.GcTCvN.aihtDYfwEo-0ycnd9JpGaT7mJ5JzOuZxTXkCNs',
+	'TOKEN': 'BOT_TOKEN',
 	'color': 0x95c4fc,
-	'avatar_bot': 'https://cdn.discordapp.com/avatars/1193585776227795065/d389318ae3dde1d0aaac065e4631addd.png?size=1024',
-	'support_server': 'https://discord.com/invite/F72VapQuQ4',
-	'add_bot': 'https://discord.com/api/oauth2/authorize?client_id=1193585776227795065&permissions=8&scope=bot',
+	'avatar_bot': 'https://discord.com/',
+	'support_server': 'https://discord.com/',
+	'add_bot': 'https://discord.com/',
 	'emoji_money': '💰'
 }
